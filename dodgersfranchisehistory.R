609bfbab2de06924fb532b52d3b8ee72efcd01b1
haroldlanham <100055668+haroldlanham@users.noreply.github.com>
2022-02-23 13:06:20 -0500

    Add files via upload

--- dodgersfranchisehistory.R
+++ dodgersfranchisehistory.R
@@ -1,5 +1,5 @@
 # Harold Lanham and Kathryn Nuttall
-# Draft Date 19 Feb 2022
+# Draft Date 23 Feb 2022
 
 # Why does a pitcher raise one leg when he throws the ball?
 # If he raised them both, he’d fall down.
@@ -19,22 +19,22 @@ hitting <- read_csv("https://github.com/haroldlanham/history_of_dodgers_franchis
 pitching <- read_csv("https://github.com/haroldlanham/history_of_dodgers_franchise/raw/main/dodgers_pitching.csv")
 
 # Bubble plot please work
-hitting %>%
+fh %>%
   arrange(desc(Year)) %>%
-  mutate(BB = BB/AB)
-ggplot(hitting, aes(x=BB, y=AB, size = Year, color = OPS)) +
+  mutate(W_L = W/L)
+ggplot(fh, aes(x=W, y=L, size = Year, color = W_L)) +
   geom_point(alpha=0.5, color="blue") +
-  scale_size(range = c(.1, 5), name="Batting") +
+  scale_size(range = c(.1, 5), name="Wins_Losses") +
   scale_color_ft() +
   theme_ipsum() +
   theme(legend.position="bottom") +
-  ylab("AB") +
-  xlab("BB") +
+  ylab("L") +
+  xlab("W") +
   theme(legend.position = "none")
 
 labs(title = 'Year: {frame_time}', 
-     x = 'BB', 
-     y = 'AB') +
+     x = 'W', 
+     y = 'L') +
   transition_time(Year) +
   ease_aes('linear')
 
@@ -42,4 +42,4 @@ print(summary(fh))
 print(summary(hitting))
 print(summary(pitching))
 
-anim_save(animation = p, "hitting.gif")
+anim_save(animation = p, "franchise_w_l.gif")
